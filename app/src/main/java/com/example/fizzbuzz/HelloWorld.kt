c7be31cc9a6e6cbb125d6321b5c2a796683d9984
Elliot Barnes <Elliotbarnes123@gmail.com>
2020-07-06 15:04:15 +0100

    FizzBuzz v1

--- app/src/main/java/com/example/fizzbuzz/HelloWorld.kt
+++ app/src/main/java/com/example/fizzbuzz/HelloWorld.kt
@@ -1,5 +1,17 @@
 package com.example.fizzbuzz
 
 fun main(){
-    print("Hello World")
+    for (i in 1..200){
+        var output = ""
+        if (i % 3 == 0){
+            output += "Fizz"
+        }
+        if (i % 5 == 0){
+            output += "Buzz"
+        }
+        if (output == ""){
+            output = i.toString()
+        }
+        println(output)
+    }
 }
