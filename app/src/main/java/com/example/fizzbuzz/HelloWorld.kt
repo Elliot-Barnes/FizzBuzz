e68ad48f02b4ffaa0814b635bfcd5ef35fbc529d
Elliot Barnes <Elliotbarnes123@gmail.com>
2020-07-06 16:31:29 +0100

    FizzBuzz v4

--- app/src/main/java/com/example/fizzbuzz/HelloWorld.kt
+++ app/src/main/java/com/example/fizzbuzz/HelloWorld.kt
@@ -2,41 +2,37 @@ package com.example.fizzbuzz
 
 fun main(){
     val numberOfRules = 5 //17 doesn't add anything so ignore that
-    for (i in 1..1785){
+    for (i in 1..1786){
         // a flag to check if the number passes any of the checks
         var divisible = false
         val output = Array(numberOfRules) {""}
-        if (i % 3 == 0){
-            output[0] = "Fizz"
-            divisible = true
-        }
+        val rules = listOf(3,13,5,7) //11 works differently
+        val words = listOf("Fizz","Fezz", "Buzz", "Bang")
+        val result = rules.map { j -> i%j==0 }
 
-        if (i % 5 == 0){
-            output[2] = "Buzz"
-            divisible = true
+        for (k in 0..3){
+            if (result[k]) {
+                output[k] = words[k]
+                divisible = true
+            }
         }
-        if (i % 7 == 0){
-            output[3] = "Bang"
-            divisible = true
-        }
-        //Bong should be printed alone
+        //bong should be printed alone or after fezz
         if (i % 11 == 0){
-            for (j in 0..4) output[j] = ""
-            output[4] = "Bong"
+            output[0] = ""
+            //Leaves Fezz alone
+            output[2] = "Bong"
+            output[3] = ""
             divisible = true
         }
-        if (i % 13 == 0) {
-            output[1] = "Fezz"
-            divisible = true
-        }
-
-        //Reverses the output
+        //reverses if divisible by 17
         if (i % 17 == 0){
             output.reverse()
         }
-        if (!divisible){
-            output[0] = i.toString()
+        if (divisible){
+            println(output.joinToString(""))
+        } else {
+            println(i)
         }
-        println(output.joinToString(""))
+
     }
 }
