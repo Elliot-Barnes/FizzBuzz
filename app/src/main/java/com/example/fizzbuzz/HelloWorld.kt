75a8a85d73b828413af74b7af687448a47ecce08
Elliot Barnes <Elliotbarnes123@gmail.com>
2020-07-06 15:22:19 +0100

    FizzBuzz v2

--- app/src/main/java/com/example/fizzbuzz/HelloWorld.kt
+++ app/src/main/java/com/example/fizzbuzz/HelloWorld.kt
@@ -1,13 +1,34 @@
 package com.example.fizzbuzz
 
 fun main(){
-    for (i in 1..100){
+    for (i in 1..300){
         var output = ""
-        if (i % 3 == 0){
-            output += "Fizz"
-        }
         if (i % 5 == 0){
             output += "Buzz"
+        }
+        if (i % 7 == 0){
+            output += "Bang"
+        }
+        //Bong should be printed alone
+        if (i % 11 == 0){
+            output = "Bong"
+        }
+        //Fezz should be printed infront of all the B's
+        if (i % 13 == 0) {
+            output = "Fezz" + output
+        }
+        //Fizz should be printed first unless bong is present
+        if (i % 3 == 0 && i % 11 != 0){
+            output = "Fizz" + output
+        }
+        //Takes the last 4 characters and
+        if (i % 17 == 0){
+            var reversedOutput = ""
+            while (output != ""){
+                reversedOutput += output.subSequence(output.length-4,output.length)
+                output = output.subSequence(0,output.length - 4).toString()
+            }
+            output = reversedOutput
         }
         if (output == ""){
             output = i.toString()
