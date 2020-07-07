b06c2e5883f0620a472fa1b8427f7b9d89e5b54e
Elliot Barnes <Elliotbarnes123@gmail.com>
2020-07-07 09:50:15 +0100

    FizzBuzz v5, Now with higher order functions

--- app/src/main/java/com/example/fizzbuzz/HelloWorld.kt
+++ app/src/main/java/com/example/fizzbuzz/HelloWorld.kt
@@ -1,37 +1,42 @@
 package com.example.fizzbuzz
 
-fun main(){
-    val numberOfRules = 5 //17 doesn't add anything so ignore that
-    for (i in 1..1786){
-        // a flag to check if the number passes any of the checks
-        var divisible = false
-        val output = Array(numberOfRules) {""}
-        val rules = listOf(3,13,5,7) //11 works differently
-        val words = listOf("Fizz","Fezz", "Buzz", "Bang")
-        val result = rules.map { j -> i%j==0 }
-
-        for (k in 0..3){
-            if (result[k]) {
-                output[k] = words[k]
-                divisible = true
+import android.provider.UserDictionary
+//returns a function that does the normal mod rules
+fun constructModRule(words: List<String>, rules: List<Int>): (Int, Array<String>) -> Array<String> {
+    return (fun(i: Int, output: Array<String>):Array<String>{
+        for (j in 0..rules.size-1) {
+            if (i%rules[j] == 0){
+                output[j] = words[j]
             }
         }
+        return output
+    })
+}
+
+fun main(){
+    val numberOfRules = 5 //17 doesn't add anything so ignore that
+    for (i in 1..105){
+        // a flag to check if the number passes any of the checks
+        var output = Array(numberOfRules) {""}
+        val rules = listOf(3,13,5,7) //11 works differently
+        val words = listOf("Fizz","Fezz", "Buzz", "Bang")
+        output = constructModRule(words,rules)(i,output)
+
         //bong should be printed alone or after fezz
         if (i % 11 == 0){
             output[0] = ""
             //Leaves Fezz alone
             output[2] = "Bong"
             output[3] = ""
-            divisible = true
         }
         //reverses if divisible by 17
         if (i % 17 == 0){
             output.reverse()
         }
-        if (divisible){
-            println(output.joinToString(""))
-        } else {
+        if (output.contentEquals(Array(numberOfRules){""})){
             println(i)
+        } else {
+            println(output.joinToString(""))
         }
 
     }
