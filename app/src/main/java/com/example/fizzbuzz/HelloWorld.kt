3b0507fcf970ad2f2e7e39ba0bb63b18224b82ef
Elliot Barnes <Elliotbarnes123@gmail.com>
2020-07-07 10:57:05 +0100

    FizzBuzz v6, Now with even higher order functions

--- app/src/main/java/com/example/fizzbuzz/HelloWorld.kt
+++ app/src/main/java/com/example/fizzbuzz/HelloWorld.kt
@@ -2,42 +2,41 @@ package com.example.fizzbuzz
 
 import android.provider.UserDictionary
 //returns a function that does the normal mod rules
-fun constructModRule(words: List<String>, rules: List<Int>): (Int, Array<String>) -> Array<String> {
-    return (fun(i: Int, output: Array<String>):Array<String>{
-        for (j in rules.indices) {
-            if (i%rules[j] == 0){
-                output[j] = words[j]
-            }
+fun constructModRule(rule: Pair<Int, String>): (Int) -> String {
+    return (fun(i: Int): String {
+        return if (i% rule.first == 0){
+            rule.second
+        } else {
+            ""
         }
-        return output
     })
 }
 
 fun main(){
-    val numberOfRules = 5 //17 doesn't add anything so ignore that
     val rules = listOf(3,13,5,7) //11 works differently
     val words = listOf("Fizz","Fezz", "Buzz", "Bang")
-    val modRule = constructModRule(words,rules)
-    for (i in 1..105){
-        // a flag to check if the number passes any of the checks
-        var output = Array(numberOfRules) {""}
-        output = modRule(i,output)
+    val modRules = rules zip words
 
+    for (i in 1..1785){
+        var output = modRules.map{j-> constructModRule(j)(i) }
         //bong should be printed alone or after fezz
         if (i % 11 == 0){
-            output[0] = ""
-            //Leaves Fezz alone
-            output[2] = "Bong"
-            output[3] = ""
+            output = if (i % 13 == 0) {
+                listOf("Fezz")
+            } else{
+                listOf()
+            }
+            output = output + "Bong"
         }
         //reverses if divisible by 17
         if (i % 17 == 0){
-            output.reverse()
+            output = output.asReversed()
         }
-        if (output.contentEquals(Array(numberOfRules){""})){
+        val outputString = output.joinToString("")
+        if (outputString == ""){
             println(i)
         } else {
-            println(output.joinToString(""))
+            println(outputString)
         }
 
     }
