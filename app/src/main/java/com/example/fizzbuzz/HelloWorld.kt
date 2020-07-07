40bbee54cc5c3ebb2d5549e0b1537f13ab8f4287
Elliot Barnes <Elliotbarnes123@gmail.com>
2020-07-07 12:49:49 +0100

    FizzBuzz v8, Generalised the rules

--- app/src/main/java/com/example/fizzbuzz/HelloWorld.kt
+++ app/src/main/java/com/example/fizzbuzz/HelloWorld.kt
@@ -1,37 +1,62 @@
 package com.example.fizzbuzz
 
 import android.provider.UserDictionary
+import java.text.FieldPosition
+import kotlin.reflect.typeOf
+
 //returns a function that does the normal mod rules
-fun constructModRule(rule: Pair<Int, String>): (Int) -> String {
+fun constructModRule(rule:Int, word:String): (Int) -> String {
     return (fun(i: Int): String {
-        return if (i% rule.first == 0){
-            rule.second
+        return if (i% rule == 0){
+            word
         } else {
             ""
         }
     })
 }
+// rule for 11
+fun constructModReplaceRule(rule: Int, word: String): (Int, List<String>) -> List<String> {
+    return (fun(i: Int, output: List<String>): List<String> {
+        return if (i % rule == 0){
+            // 11 rule keeps fezz but removes everything else
+            val out = if (i % 13 == 0){
+                listOf("Fezz")
+            } else {
+                listOf()
+            }
+            out + word
+        } else {
+            output
+        }
+    })
+}
+
+fun constructModReverseRule(rule: Int): (Int, List<String>) -> List<String> {
+    return (fun(i: Int, output: List<String>): List<String> {
+        return if(i % rule == 0){
+            output.asReversed()
+        } else {
+            output
+        }
+    })
+}
 
 fun main(){
-    val rules = listOf(3,13,5,7) //11 works differently
-    val words = listOf("Fizz","Fezz", "Buzz", "Bang")
-    val modRules = rules zip words
-    val ruleList = modRules.map { j-> constructModRule(j) }
+    var ruleList = listOf<(Int)->String>()
+    var replaceRuleList = listOf<(Int, List<String>)-> List<String>>()
+    var reverseRuleList = listOf<(Int, List<String>)-> List<String>>()
+    ruleList = ruleList + constructModRule(3,"Fizz")
+    ruleList = ruleList + constructModRule(13,"Fezz")
+    ruleList = ruleList + constructModRule(5,"Buzz")
+    ruleList = ruleList + constructModRule(7, "Bang")
+    replaceRuleList = replaceRuleList + constructModReplaceRule(11, "Bong")
+    reverseRuleList = reverseRuleList + constructModReverseRule(17)
     for (i in 1..1785){
         var output = ruleList.map{f -> f(i)}
         //bong should be printed alone or after fezz
-        if (i % 11 == 0){
-            output = if (i % 13 == 0) {
-                listOf("Fezz")
-            } else{
-                listOf()
-            }
-            output = output + "Bong"
-        }
+        output = replaceRuleList.fold(output,{ x,rule -> rule(i,x)})
         //reverses if divisible by 17
-        if (i % 17 == 0){
-            output = output.asReversed()
-        }
+        output = reverseRuleList.fold(output,{x,f -> f(i,x) })
         val outputString = output.joinToString("")
         if (outputString == ""){
             println(i)
