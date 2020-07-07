ccf2e8dafd9473798fcf1764693de14f6c32f511
Elliot Barnes <Elliotbarnes123@gmail.com>
2020-07-07 14:02:36 +0100

    FizzBuzz v10, Changed the type signatures to match and merged all the calls

--- app/src/main/java/com/example/fizzbuzz/HelloWorld.kt
+++ app/src/main/java/com/example/fizzbuzz/HelloWorld.kt
@@ -1,16 +1,12 @@
 package com.example.fizzbuzz
 
-import android.provider.UserDictionary
-import java.text.FieldPosition
-import kotlin.reflect.typeOf
-
 //returns a function that does the normal mod rules
-fun constructModRule(rule:Int, word:String): (Int) -> String {
-    return (fun(i: Int): String {
+fun constructModRule(rule:Int, word:String): (Int, List<String>) -> List<String> {
+    return (fun(i: Int, output: List<String>): List<String> {
         return if (i% rule == 0){
-            word
+            output + word
         } else {
-            ""
+            output
         }
     })
 }
@@ -49,23 +45,15 @@ fun constructInsertionModRule(rule: Int, word: String): (Int, List<String>) -> L
 }
 
 fun main(){
-    var ruleList = listOf<(Int)->String>()
-    var replaceRuleList = listOf<(Int, List<String>)-> List<String>>()
-    var reverseRuleList = listOf<(Int, List<String>)-> List<String>>()
-    var insertRuleList = listOf<(Int, List<String>)-> List<String>>()
-    ruleList = ruleList + constructModRule(3,"Fizz")
-    ruleList = ruleList + constructModRule(5,"Buzz")
-    ruleList = ruleList + constructModRule(7, "Bang")
-    replaceRuleList = replaceRuleList + constructModReplaceRule(11, "Bong")
-    insertRuleList = insertRuleList + constructInsertionModRule(13, "Fezz")
-    reverseRuleList = reverseRuleList + constructModReverseRule(17)
-
-    for (i in 1..143){
-        var output = ruleList.map{f -> f(i)}
-        output = replaceRuleList.fold(output,{ x,rule -> rule(i,x)})
-        output = insertRuleList.fold(output,{x,rule -> rule(i,x)})
-        //reverses if divisible by 17
-        output = reverseRuleList.fold(output,{x,f -> f(i,x) })
+    val ruleList = listOf(constructModRule(3,"Fizz"),
+        constructModRule(5,"Buzz"),
+        constructModRule(7, "Bang"),
+        constructModReplaceRule(11, "Bong"),
+        constructInsertionModRule(13, "Fezz"),
+        constructModReverseRule(17)
+    )
+    for (i in 1..1785){
+        val output = ruleList.fold(listOf(),{ x:List<String>, rule -> rule(i,x)})
         val outputString = output.joinToString("")
         if (outputString == ""){
             println(i)
